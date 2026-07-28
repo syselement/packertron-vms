d15f67df2f7aea5bf7833274f22fcebfaa64eebf
syselement <81392234+syselement@users.noreply.github.com>
2026-07-28 22:42:31 +0200

    feat: add de.swsnr.turnon Flatpak package to desktop environment setup

--- scripts/ubuntu/03-customize-system.sh
+++ scripts/ubuntu/03-customize-system.sh
@@ -124,6 +124,7 @@ readonly -a DESKTOP_PACKAGES=(
 
 readonly -a FLATPAK_PACKAGES=(
     com.bitwarden.desktop
+    de.swsnr.turnon
     io.ente.auth
     io.github.sigmasd.pingmonitor
     org.cryptomator.Cryptomator
